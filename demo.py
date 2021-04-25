0bd6cb9a6fe3f1d78af07eaafcc0358cfd077f85
Aditya Singh <adityasingh.welcome@gmail.com>
2021-04-25 17:43:52 +0530

    new

--- demo.py
+++ demo.py
@@ -1,13 +1,2 @@
-# # p = "aditya singh"
-# letter = input("enter what you want to do: ")
-# if "cinema" not in letter.casefold():
-#     print("i want to go cinema")
-# else:
-#     print("whaaaoooo")
-# num = "9,456:649 562;665;563"
-# seprator = ""
-# for char in num:
-#     if not char.isnumeric():
-#         seprator = seprator + char
-#         print(seprator)
-# jkjhkjl
+for i in range(2, 10, 2):
+ print("i is now {}" .format(i))
